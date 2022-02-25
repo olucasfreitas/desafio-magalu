5ab82f50a1d387a1586dacc545689b097df2f2ae
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-02-25 00:26:29 -0300

    conected bacend with database and fixed login route

--- back/app.js
+++ back/app.js
@@ -1,19 +1,36 @@
 const express = require("express");
 const cors = require("cors");
+const mysql = require("mysql2");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "mysql",
+  database: "db_magalu",
+  port: 3306,
+});
+
 app.post("/sign-in", (request, response) => {
   const { username, password } = request.body;
 
-  if (username === "magalu" && password === "m@galu123") {
-    const user = { username, password };
-    return response.json(user);
-  } else {
-    return response.status(404).json({ message: "Erro ao logar" });
-  }
+  db.query(
+    "SELECT * FROM `users` WHERE `username` = ? AND `password` = ?",
+    [username, password],
+    function (err, results) {
+      if (err) {
+        return response.status(500).json({ error: err });
+      }
+      if (results.length > 0) {
+        return response.status(201).json(results);
+      } else {
+        return response.status(500).json({ message: "Senha o email errados" });
+      }
+    }
+  );
 });
 
 app.listen(8002, () => console.log("Servidor está rodando na porta 8002"));
